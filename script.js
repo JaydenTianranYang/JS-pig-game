c5dcc5b2f05967617b94fe90fa5154e7774f14c3
Jayden Yang <jayden.yang@rea-group.com>
2025-03-21 22:17:47 +1100

    implement the logic of switching the player

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
 let currentScore = 0;
+let activePlayer = 0;
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
@@ -31,8 +32,9 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     // Add dice to current score
     currentScore += dice;
-    current0El.textContent = currentScore; // CHANGE LATER
+    document.querySelector(`#current--${activePlayer}`).textContent = currentScore;
   } else {
     // Switch to next player
+    activePlayer = activePlayer === 0 ? 1 : 0;
   }
 });
